f5dc68b0a82a0ba280932873942d02e318ffa7f7
Jose Bermeo <josedavid@bunnyinc.com>
2017-09-21 07:46:23 -0500

    New architecture

--- model.py
+++ model.py
@@ -11,33 +11,33 @@ from keras import backend as K
 yuv_transfor = K.constant([[.299, .587, .114],
                          [-.14713, -.28886, .436],
                          [.615, -.51499, -.10001]], shape=(3,3), dtype="float32")
-yuv_mu = K.constant([-128.0, .0, .0], shape=(3,), dtype="float32")
-yuv_sigma = K.constant([128.0, 256.0*.436, 256.0*.615], shape=(3,), dtype="float32")
+yuv_mu = K.constant([-128., .0, .0], shape=(3,), dtype="float32")
+yuv_sigma = K.constant([128., 256.*.436, 256.*.615], shape=(3,), dtype="float32")
 
 def pre_process(x):
     """Create YUV planes and grayscale and concatenate them"""
     grayscale = K.mean(x, [3], keepdims=True)
-    grayscale = (grayscale - 128.0) / 128.0
+    grayscale = (grayscale - 128.) / 128.
 
-    # yuv = K.dot(x, yuv_transfor) + yuv_mu / yuv_mu
-    # return K.concatenate([yuv, grayscale])
-    #return grayscale
+    yuv = K.dot(x, K.transpose(yuv_transfor)) + yuv_mu / yuv_mu
+    return K.concatenate([yuv, grayscale])
 
 
 def build_model(input_shape):
     """Build sequential model based on NVIDIA architecture."""
     model = Sequential()
     model.add(Cropping2D(((50,20), (0,0)), input_shape=input_shape))
-    model.add(Lambda(lambda x: K.resize_images(x, .5, .5, "channels_last")))
     model.add(Lambda(pre_process))
     model.add(Conv2D(24, [5, 5], padding="valid", activation="relu"))
     model.add(MaxPooling2D())
     model.add(Conv2D(36, [5, 5], padding="valid", activation="relu"))
     model.add(MaxPooling2D())
+    model.add(Conv2D(48, [5, 5], padding="valid", activation="relu"))
+    model.add(MaxPooling2D())
     # Dropout layers for better generalization
     model.add(Dropout(.1))
-    model.add(Conv2D(48, [3, 3], padding="valid", activation="relu"))
-    model.add(MaxPooling2D())
+    model.add(Conv2D(64, [3, 3], padding="valid", activation="relu"))
+    model.add(Conv2D(64, [3, 3], padding="valid", activation="relu"))
     model.add(Conv2D(64, [3, 3], padding="valid", activation="relu"))
     # Dropout layers for better generalization
     model.add(Dropout(.1))
@@ -103,25 +103,25 @@ if __name__ == "__main__":
     model.compile(loss="mse", optimizer="adam")
     model.summary()
 
-    # # Build dataset, python list of tuples.
-    # # Each tuple is a file_path, image_trasformations, steering, steering_trasformations
-    # dataset = build_fit_dataset(args.data_path)
-    # # Split dataset in train and validation
-    # train_dataset, test_dataset = train_test_split(dataset, test_size=.2)
-    # # Create sequences for parallel access to the dataset.
-    # # Generator will read image from file, transform it returned for training.
-    # train_sequence = Sequence(train_dataset, args.batch_size)
-    # test_sequence = Sequence(test_dataset, args.batch_size)
-    #
-    # # Train the model
-    # model.fit_generator(
-    #     train_sequence, len(train_sequence),
-    #     epochs=args.epochs,
-    #     validation_data=test_sequence, validation_steps=len(test_sequence),
-    #     max_queue_size=10, workers=6,
-    #     verbose=2)
-    #
-    # # Store final model and weights
-    # model.save(args.model_output)
-    # name, ext = os.path.splitext(args.model_output)
-    # model.save(name + "_weights" + ext)
+    # Build dataset, python list of tuples.
+    # Each tuple is a file_path, image_trasformations, steering, steering_trasformations
+    dataset = build_fit_dataset(args.data_path)
+    # Split dataset in train and validation
+    train_dataset, test_dataset = train_test_split(dataset, test_size=.2)
+    # Create sequences for parallel access to the dataset.
+    # Generator will read image from file, transform it returned for training.
+    train_sequence = Sequence(train_dataset, args.batch_size)
+    test_sequence = Sequence(test_dataset, args.batch_size)
+
+    # Train the model
+    model.fit_generator(
+        train_sequence, len(train_sequence),
+        epochs=args.epochs,
+        validation_data=test_sequence, validation_steps=len(test_sequence),
+        max_queue_size=10, workers=6,
+        verbose=2)
+
+    # Store final model and weights
+    model.save(args.model_output)
+    name, ext = os.path.splitext(args.model_output)
+    model.save(name + "_weights" + ext)
